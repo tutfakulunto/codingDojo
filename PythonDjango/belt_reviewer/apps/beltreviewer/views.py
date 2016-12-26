ffec4ef86d192079503a047b4ecef263707034ce
Scott Johnson <c.scott.johnson@gmail.com>
2016-12-26 18:12:26 +0700

    Fixed problem with views files

--- PythonDjango/belt_reviewer/apps/beltreviewer/views.py
+++ PythonDjango/belt_reviewer/apps/beltreviewer/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from models import Book, Author, Review
+from ..login_reg_app.views import print_messages
+from ..login_reg_app.models import User
 
 # Create your views here.
+from django.template import Library
+register = Library()
+
+def check_logged_in(request):
+    return 'user' in request.session
+
+def index(request):
+    if not check_logged_in(request):
+        return redirect(reverse('login_reg:index'))
+
+    context = {
+        'reviews': Review.objects.fetch_recent(),
+        'books': Book.objects.all()
+    }
+
+    return render(request, 'beltreviewer/index.html', context)
+
+def new(request):
+    if not check_logged_in(request):
+        return redirect(reverse('login_reg:index'))
+
+    result = Review.objects.create_review(request.POST, request.session['user']['id'])
+
+    if result[0] == True:
+        return redirect(reverse('reviewer:show', kwargs={'id': result[1].book.id}))
+    else:
+        print_messages(request, result[1])
+        return redirect(reverse('reviewer:new'))
+
+@register.filter
+def show(request, id):
+    if not check_logged_in(request):
+        return redirect(reverse('login_reg:index'))
+
+    book = Book.objects.get(id=id)
+    return render(request, 'beltreviewer/show.html', { 'book' : book })
+
+@register.filter
+def show_user(request, id):
+    if not check_logged_in(request):
+        return redirect(reverse('login_reg:index'))
+
+    user = User.objects.fetch_user_info(id)
+    return render(request, 'beltreviewer/show_user.html', { 'user' : user })
--- PythonDjango/belt_reviewer/apps/login_registration/views.py
+++ PythonDjango/belt_reviewer/apps/login_registration/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from models import User
 
 # Create your views here.
+def index(request):
+    return render(request, 'login_registration/index.html')
+
+def login(request):
+    result = User.objects.validateLogin(request)
+
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('login_reg:index'))
+
+    return log_user_in(request, result[1])
+
+def register(request):
+    result = User.objects.validateReg(request)
+
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('login_reg:index'))
+
+    return log_user_in(request, result[1])
+
+def success(request):
+    if not 'user' in request.session:
+        return redirect(reverse('login_reg:index'))
+
+    """
+    For this app, success means allowing the user into book reviews
+    """
+    return redirect(reverse('reviewer:index'))
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.INFO, message)
+
+def log_user_in(request, user):
+    request.session['user'] = {
+        'id' : user.id,
+        'name' : user.name,
+        'alias' : user.alias,
+        'email' : user.email,
+    }
+    return redirect(reverse('login_reg:success'))
+
+def logout(request):
+    request.session.pop('user')
+    return redirect(reverse('login_reg:index'))
