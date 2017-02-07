9e09cd946dc535e41153f13ad77d9cd9d03afb67
Scott Johnson <c.scott.johnson@gmail.com>
2017-02-07 13:34:43 +0700

    Fixed bug on drop down options in form for Survey Form assignment in Node

--- Node/surveyForm/routes/index.js
+++ Node/surveyForm/routes/index.js
@@ -5,8 +5,8 @@ module.exports = function Route(app){
     app.post('/result', function(request, response){
         submitted_information = {
             name: request.body.name,
-            dojoLocation: request.body.dojo_location,
-            favoriteLanguage: request.body.favorite_language,
+            dojo_location: request.body.dojo_location,
+            favorite_language: request.body.favorite_language,
             comment: request.body.comment
         };
         response.render("result", {user_data: submitted_information})
