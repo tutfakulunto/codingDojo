9d94cf65908c727f39b5175444137ea0bfb20a72
Scott Johnson <c.scott.johnson@gmail.com>
2017-02-14 16:01:40 +0700

    Finished Angular Modularize assignment

---
+++ Angular/Modularize/clients/factories/angular_user_factory.js
@@ -0,0 +1,45 @@
+app.factory('userFactory', [function() {
+    function UserConstructor() {
+
+    var users = [{
+      name: "mike"
+    }];
+
+    function typeIs(variable, type) {
+      return (typeof variable === type);
+    }
+
+    this.index = function(cb) {
+      if(!typeIs(cb, 'function')) return;
+
+      cb(users);
+    };
+    
+    this.create = function(newUser, cb) {
+      if(typeIs(newUser, 'object')) users.push(newUser);
+
+      if(typeIs(cb, 'function')) cb(users);
+    };
+   
+    this.update = function(updateUser, idx, cb) {
+     
+      if (users[idx]) {
+        for (var key in updateUser) {
+          users[idx][key] = updateUser[key];
+        }
+      }
+      if (typeIs(cb, 'function')) cb(users[idx]);
+    }
+    this.show = function(idx, cb) {
+      if (typeIs(cb, 'function')) cb(users[idx]);
+    }
+    this.delete = function(idx, cb) {
+      if (users[idx]) {
+        users.splice(idx, 1);
+      }
+      if (typeIs(cb, 'function')) cb(users);
+    }
+  }
+
+  return (new UserConstructor());
+}]);
